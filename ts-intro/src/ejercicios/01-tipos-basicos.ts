c7800941b62a6c93c10fb640f2d78ec9a0e85eae
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-07-19 14:38:17 -0500

    Primeros ejercicios de typescript

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string         = 'Strider',
+    hp    : number| string = 95,
+    estaVivo: boolean      = true;
+    
+hp = 'FULL';
+
+console.log(nombre, hp);
