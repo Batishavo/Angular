32e6dc6e2eb061a2ab236a7d4fe5bbebb84965bb
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-07-22 18:05:49 -0500

    Miniatura de la clase 6:14 / 6:15 45. Crear componente de forma automática

---
+++ angular/01-bases/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeroeComponent } from './heroes/heroe/heroe.components';
+import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent,
+    ListadoComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ angular/01-bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent  {
+
+  constructor() {
+
+    console.log('contructor');
+
+  }
+
+}
