817dde4b4de74af055b5cc3ac291c120cb0447b2
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-08-04 18:49:25 -0500

    video 89

---
+++ angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchGifsResponce, Gif } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey: string ='0MvYF9rtiPMeYoVceDKiJyduOSFlLYLI';
+  private _historial: string[]=[];
+
+  //Todo: Cambiar any por su tipo correspondiente 
+  public resultados: Gif[] = [];
+
+
+  public get historial(){
+    
+    return [...this._historial];
+
+  }
+
+  constructor(private _http: HttpClient) {}
+
+  buscarGifs(query: string=''){
+
+   query = query.trim().toLowerCase();
+
+    if(! this._historial.includes(query)){
+      
+      this._historial.unshift(query);
+      
+      this._historial =this._historial.splice(0,10);
+    }
+
+    this._http.get<SearchGifsResponce>(`https://api.giphy.com/v1/gifs/search?api_key=0MvYF9rtiPMeYoVceDKiJyduOSFlLYLI&q=${query}&limit=10`)
+    .subscribe((res) => {
+       
+      //console.log(res.data);
+
+      this.resultados = res.data;     
+      //Response.data[0]. 
+    });
+    
+    //console.log(this._historial);
+
+  }
+}
--- angular/02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ angular/02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
+  constructor(private GifsService: GifsService) { }
 
-  ngOnInit(): void {
+  get hisotiral(){
+
+    return this.GifsService.historial;
+
   }
+
+ 
 
 }
