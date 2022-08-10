13b1dffdc2e599c56dffcb4260d2d7d845a171ee
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-08-10 17:31:21 -0500

    video 101

--- angular/03-paisesApp/src/app/app.module.ts
+++ angular/03-paisesApp/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule, 
+    AppRoutingModule,
+    PaisModule, 
+    SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
