12af6fd6618ad149cfb70046688ab52d379bc334
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-08-19 16:59:56 -0500

    video 135

---
+++ angular/04-pipes/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  nombre:string = 'Fernando xd';
+  valor: number = 1000;
+  obj = {
+    nombre:'Fernando'
+  }
+
+  mostrarNombre(){
+
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+    
+  }
+
+
+}
