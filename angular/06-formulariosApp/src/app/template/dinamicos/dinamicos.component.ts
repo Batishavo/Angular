6cbcf52519c989a72abdbb84bf9f0d1abef9c35b
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-08-30 19:20:40 -0500

    secion 19

--- angular/06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ angular/06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,17 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona{
+  nombre:string;
+  favoritos:Favorito[];
+}
+
+interface Favorito{
+  nombre:string;
+  id:number;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
 
+export class DinamicosComponent implements OnInit {
+  nuevoJuego:string ="";
+  persona: Persona={
+    nombre: 'Fernando',
+    favoritos:[
+      {
+        id:1,
+        nombre:'Metal Gear'
+      },
+      {
+        id:2,
+        nombre:'Metal Gear 2'
+      }
+    ]
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
   guardar(){
     console.log('Formulario posteado');
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index,1)
+  }
+  agregar(){
+    const juego: Favorito ={
+      id: this.persona.favoritos.length +1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({...juego});
+    this.nuevoJuego = '';
   }
 }
--- angular/06-formulariosApp/src/app/template/switches/switches.component.ts
+++ angular/06-formulariosApp/src/app/template/switches/switches.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
+  persona={
+    cromosoma: '',
+    notificaciones: true,
+  }
+
+  terminoasYCondiciones: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
